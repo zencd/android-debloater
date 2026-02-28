1949472822e24d016a28e0d683c241083b513eb2
zzz <z@z.com>
2026-03-01 01:45:33 +0300

    ExtractApkMeta

--- web.py
+++ web.py
@@ -151,8 +151,6 @@ PORT = 59861  # (55000..60999] is ok
 # PROFILE_DIR = APP_HOME / 'profile-empty'
 PROFILE_DIR = APP_HOME / 'profile1'
 # PROFILE_DIR = APP_HOME / 'profile-mama-12x'
-APP_START_DIR = Path.cwd()
-# APKS_DIR = APP_START_FOLDER / 'apks_pv'
 APKS_DIR = PROFILE_DIR / 'apk'
 ALL_PERMISSIONS_FILE = PROFILE_DIR / 'permissions.txt'
 
@@ -600,7 +598,7 @@ def backup_apks_for_package(package: str) -> bool:
     apk_paths = list_apk_paths_on_device(package)
     if not apk_paths:
         return False
-    with tempfile.TemporaryDirectory(dir=APP_START_DIR) as tmp_dir_:
+    with tempfile.TemporaryDirectory() as tmp_dir_:
         tmp_dir = Path(tmp_dir_)
         cwd_before = os.getcwd()
         try:
@@ -935,100 +933,133 @@ def serve_load_local_apps(request, response):
             'packages': packages, 'localAppsFolder': str(PROFILE_DIR)}
 
 
-def serve_read_device_apps_meta(request, response):
-    APP_META_FILE.load()
-    APP_META_FILE.data = APP_META_FILE.data if APP_META_FILE.data else dict()
-    app_meta_packages = APP_META_FILE.data.get('packages')
-    if not app_meta_packages:
-        app_meta_packages = dict()
-        APP_META_FILE.data['packages'] = app_meta_packages
+class ExtractApkMeta:
 
-    start_dir = os.getcwd()
-    oks, fails = 0, 0
-    try:
-        for package in list_device_all_packages():
-            if package in app_meta_packages:
-                continue
-            apk_paths = list_apk_paths_on_device(package)
-            if not apk_paths:
-                fails += 1
-                continue
-            apk_path = ''
-            if len(apk_paths) == 1:
-                apk_path = next(iter(apk_paths))
-            if not apk_path:
-                apk_paths_2 = list(filter(lambda x: x.endswith('/base.apk'), apk_paths))
-                if apk_paths_2:
-                    apk_path = apk_paths_2[0]
-            if not apk_path:
-                print(f'WARN: cannot find main apk for {package}')
-                fails += 1
-                continue
+    def __init__(self):
+        db = APP_META_FILE
+        db.load()
+        db.data = db.data if db.data else dict()
+        app_meta_packages = db.data.get('packages')
+        if not app_meta_packages:
+            app_meta_packages = dict()
+            db.data['packages'] = app_meta_packages
+        self.db = db
+        self.app_meta_packages = app_meta_packages
 
-            tmp_app_folder = APP_START_DIR / 'tmp'  # todo use a random folder
-            if tmp_app_folder.exists():
-                # todo fails on windows: file is busy
-                # todo fails on windows: file is busy
-                # todo fails on windows: file is busy
-                shutil.rmtree(tmp_app_folder)
-            tmp_app_folder.mkdir()
+    def __find_one_remote_apk(self, apk_paths):
+        apk_path = ''
+        if len(apk_paths) == 1:
+            apk_path = next(iter(apk_paths))
+        if not apk_path:
+            apk_paths_2 = list(filter(lambda x: x.endswith('/base.apk'), apk_paths))
+            if apk_paths_2:
+                apk_path = apk_paths_2[0]
+        return apk_path
+
+    def __extract_meta_from_local_apk(self, local_apk_path: Path):
+        app_title, icon_data, icon_ext = '', '', ''
+        try:
+            apk_info = pyaxmlparser.APK(str(local_apk_path))
+        except Exception as e:
+            print(f'ERROR: pyaxmlparser failed parsing {local_apk_path}')  # pyaxmlparser is buggy
+            return app_title, icon_data, icon_ext
+        app_title = resolve_apk_title(apk_info)
+        if not app_title:
+            print(f'WARN: failed resolving app title from apk {local_apk_path}, using {local_apk_path.stem}')
+            app_title = local_apk_path.stem
+        if not app_title or app_title == 'base':
+            print(f'WARN: failed resolving app title from apk {local_apk_path}, skipping')
+            return app_title, icon_data, icon_ext
+        icon_data = self.__extract_icon_data(apk_info)
+        icon_info = self.__extract_icon_path_in_apk(apk_info)
+        if icon_data and icon_info and isinstance(icon_data, bytes):
+            icon_ext = os.path.splitext(icon_info)[1]
+        return app_title, icon_data, icon_ext
+
+    def __extract_icon_data(self, apk_info: pyaxmlparser.APK):
+        try:
+            return apk_info.icon_data
+        except:
+            return b''
+
+    def __extract_icon_path_in_apk(self, apk_info: pyaxmlparser.APK):
+        try:
+            return apk_info.icon_info
+        except:
+            return ''
+
+    def __pull_apk_into_temp_folder(self, apk_path: str):
+        cwd = os.getcwd()
+        try:
+            tmp_app_folder = Path(tempfile.mkdtemp())
             os.chdir(tmp_app_folder)
             cmd = ['adb', 'pull', apk_path]
-            # todo if failed then resolve it as stem
             rc, stdout, stderr = exec_(cmd, stdout=None, stderr=None)
             if rc != 0:
-                print(f'Command failed with rc: {rc}')
-                fails += 1
-                continue
+                return False, ''
+            return True, tmp_app_folder
+        finally:
+            os.chdir(cwd)
+
+    def __persist_icon(self, icon_data, app_icon_file_short):
+        icon_file = APP_ICON_DIR / app_icon_file_short
+        ensure_dir(icon_file.parent)
+        with open(icon_file, 'wb') as fp:
+            fp.write(icon_data)
+
+    def extract(self, package: str):
+        tmp_app_folder = None  # type: Optional[Path]
+        try:
+            apk_paths = list_apk_paths_on_device(package)
+            if not apk_paths:
+                return False
+            apk_paths = list_apk_paths_on_device(package)
+            if not apk_paths:
+                return False
+            apk_path = self.__find_one_remote_apk(apk_paths)
+            if not apk_path:
+                return False
+            ok, tmp_app_folder = self.__pull_apk_into_temp_folder(apk_path)
+            if not ok:
+                return False
             local_apk_path = tmp_app_folder / os.path.basename(apk_path)
-            try:
-                apk_info = pyaxmlparser.APK(str(local_apk_path))
-            except Exception as e:
-                print(f'ERROR: pyaxmlparser failed parsing {local_apk_path}')  # pyaxmlparser is buggy
-                traceback.print_exc()
-                fails += 1
-                continue
-            app_title = resolve_apk_title(apk_info)
-            if not app_title:
-                print(f'WARN: failed resolving app title from apk {local_apk_path}, using {local_apk_path.stem}')
-                app_title = local_apk_path.stem
-            if not app_title or app_title == 'base':
-                print(f'WARN: failed resolving app title from apk {local_apk_path}, skipping')
-                fails += 1
-                continue
-            # icon = apk_info.get_app_icon()
             app_icon_file_short = ''
-            try:
-                icon_data = apk_info.icon_data
-                icon_info = apk_info.icon_info
-                if icon_data and icon_info and isinstance(icon_data, bytes):
-                    icon_ext = os.path.splitext(icon_info)[1]
-                    if icon_ext:
-                        app_icon_file_short = f'{package}{icon_ext}'
-                        icon_file = APP_ICON_DIR / app_icon_file_short
-                        ensure_dir(icon_file.parent)
-                        with open(icon_file, 'wb') as fp:
-                            fp.write(icon_data)
-            except Exception as e:
-                print(f'ERROR: Failed reading icon for {package}: {e}')
-                # traceback.print_exc()
-            print('app_title:', app_title)
-            print('  version_name:', apk_info.version_name)
-            pdata = {'title': app_title,
-                     'version_name': apk_info.version_name,
-                     'icon': app_icon_file_short}
+            app_title, icon_data, icon_ext = self.__extract_meta_from_local_apk(local_apk_path)
+            if not app_title:
+                return False
+            if icon_data and icon_ext:
+                app_icon_file_short = f'{package}{icon_ext}'
+                self.__persist_icon(icon_data, app_icon_file_short)
+            pak_data = {'title': app_title}
             if app_icon_file_short:
-                pdata['icon'] = app_icon_file_short
-            app_meta_packages[package] = pdata
-            APP_META_FILE.dump()
-            oks += 1
-        response.content_type = CT_JSON
-        return {'status': 'OK', 'oks': oks, 'fails': fails}
-    finally:
-        os.chdir(start_dir)
+                pak_data['icon'] = app_icon_file_short
+            self.app_meta_packages[package] = pak_data
+            self.db.dump()
+            return True
+        except Exception as e:
+            print(f'ERROR: Failed reading icon for {package}: {e}')
+            traceback.print_exc()
+            return False
+        finally:
+            if tmp_app_folder and tmp_app_folder.exists():
+                shutil.rmtree(tmp_app_folder)
 
 
-def static_file(fname, root='.'):
+def serve_read_device_apps_meta(request, response):
+    extractor = ExtractApkMeta()
+    oks, fails = 0, 0
+    for package in list_device_all_packages():
+        if package not in extractor.app_meta_packages:
+            ok = extractor.extract(package)
+            if ok:
+                oks += 1
+            else:
+                fails += 1
+    response.content_type = CT_JSON
+    return {'status': 'OK', 'oks': oks, 'fails': fails}
+
+
+def static_file(fname):
     with open(fname, encoding='utf-8') as fd:
         return fd.read()
 
